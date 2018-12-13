a92017e1cfc626652f0c52c4e8c83510870a2eef
しゃるしぃ <info@angelbell.jp>
2018-12-13 20:10:55 +0900

    Show picked colors

--- HueSyncClone/HueSyncClone/MainWindowViewModel.cs
+++ HueSyncClone/HueSyncClone/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -6,7 +7,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
 using HueSyncClone.Core;
+using HueSyncClone.Drawing;
 using HueSyncClone.Hue;
 using HueSyncClone.Models;
 
@@ -17,8 +20,12 @@ namespace HueSyncClone
         private bool _isAuthenticated = true;
         private bool _isConnecting = true;
         private string _imagePath;
+        private IReadOnlyList<HueLight> _lights;
+        private int _lightCount;
 
         public IHueUserNameStore HueUserNameStore { get; set; } = new FileHueUserNameStore();
+
+        public ColorSelector ColorSelector { get; set; } = new ColorSelector();
 
         public bool IsConnecting
         {
@@ -38,6 +45,14 @@ namespace HueSyncClone
             set => SetField(ref _imagePath, value);
         }
 
+        public int LightCount
+        {
+            get => _lightCount;
+            set => SetField(ref _lightCount, value);
+        }
+
+        public ObservableCollection<Color> Colors { get; } = new ObservableCollection<Color>();
+
         public ICommand OnFileSelectedCommand { get; } 
 
         public MainWindowViewModel()
@@ -49,6 +64,15 @@ namespace HueSyncClone
         private void OnFileSelected(string[] filePaths)
         {
             ImagePath = filePaths.First();
+
+            using (var bitmap = (System.Drawing.Bitmap)System.Drawing.Image.FromFile(ImagePath))
+            {
+                var colors = ColorSelector.SelectColor(bitmap, _lights.Count);
+                foreach (var color in colors)
+                {
+                    Colors.Add(Color.FromRgb(color.R, color.G, color.B));
+                }
+            }
         }
 
         private async void Initialize()
@@ -65,7 +89,17 @@ namespace HueSyncClone
                 await bridge.AuthorizeAsync("HueSyncClone", CancellationToken.None);
                 HueUserNameStore.Save(bridge.UserName);
             }
+            else
+            {
+                bridge.UserName = userName;
+            }
 
+            IsConnecting = true;
+
+            _lights = await bridge.GetLightsAsync();
+
+            LightCount = _lights.Count;
+            IsConnecting = false;
             IsAuthenticated = true;
         }
 
